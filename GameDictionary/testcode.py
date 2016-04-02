53876b27db9db18d2fdaef284556f2dc78fe2f6f
Nic Colley <nic.colley@live.com>
2016-04-02 16:54:58 -0400

    add resource website

--- GameDictionary/testcode.py
+++ GameDictionary/testcode.py
@@ -1,43 +1,25 @@
 from tkinter import *
 from tkinter import ttk
 
-def calculate(*args):
-    try:
-        value = float(feet.get())
-        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
-    except ValueError:
-        pass
-def delete():
-    for grid in root.grid_slaves():
-        grid.grid_forget()
+def buildInput(master):
+     fileInputFrame = Frame(master)
+     fileInputFrame.grid(row=0)
+     Label(fileInputFrame, text="Input file:").grid(row=0, sticky=W)
+     Entry(fileInputFrame).grid(row=0, column=1)
+
+     Label(fileInputFrame, text="Output file:").grid(row=1, sticky=W)
+     Entry(fileInputFrame).grid(row=1, column=1)
+
+
+def buildButtons(master):
+     buttonFrame = Frame(master)
+     buttonFrame.grid(row=1)
+     Button(buttonFrame, text='Testing Testing').grid(row=0)
+     Label(buttonFrame, text='Some text').grid(row=0, column=1)
+     Button(buttonFrame, text='Cancel').grid(row=1, column=1)
 
 
 root = Tk()
-root.title("Feet to Meters")
-
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
-
-feet = StringVar()
-meters = StringVar()
-
-feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(W, E))
-
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
-ttk.Button(mainframe, text="Delete", command=delete).grid(column=1, row=3, sticky=W)
-
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
-
-feet_entry.focus()
-root.bind('<Return>', calculate)
-print(list(root.grid_slaves()))
-print(root.grid_size())
+buildInput(root)
+buildButtons(root)
 root.mainloop()
